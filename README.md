f80fbbaf8232adcfcd6558072659659c08f1d10b
Abhishek Tiwari <abhishek067852@gmail.com>
2023-01-17 22:00:03 +0530

    thiskeyword

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # OOPs-in-JS
 Concepts of Object oriented programming in Javascript
+# Topics to be covered here
+OOPS JS
+Learn about this keyword (behaviour in browser and in nodeJS)
+Constructor Functions(used to create your own objects)
+Call Apply and Bind
+Prototypal inheritance
+
+
---
+++ thisusage.js
@@ -0,0 +1,76 @@
+//This keyword behaves in differently in different environments(NodeJs and Browser)
+// JS has two modes to run Strict Mode and Non Strict Mode
+
+//this keyword in node with non strict mode
+// console.log(this)
+// function->log
+// object->function
+// object->function->function
+// console.log(this); // return an empty object
+
+
+// function showThis()
+// {
+//     console.log(this);
+// }
+// showThis(); //return a global object which having all details
+
+// let obj={
+//     name:"adm",
+//     f:function(){
+       
+//             console.log(this);
+      
+//     }
+// }
+// obj.f(); // return complete object itself
+
+// let obj={
+//     name:"adm",
+//     f:function(){
+//         function g()
+//         {
+//             console.log(this);
+//         }
+//         g();
+//     }
+// }
+// obj.f(); // return global object 
+
+
+//this keyword in node with strict mode
+'use strict'
+// console.log(this) //return empty
+// function showThis()
+// {
+//     console.log(this);
+// }
+// showThis(); //return undefined React by default works in strict mode
+
+// let obj={
+//     name:"adm",
+//     f:function(){
+       
+//             console.log(this);
+      
+//     }
+// }
+// obj.f(); // return complete object itself
+
+// let obj={
+//     name:"adm",
+//     f:function(){
+//         function g()
+//         {
+//             console.log(this);
+//         }
+//         g();
+//     }
+// }
+// obj.f(); // return global object 
+
+
+
+
+
+
